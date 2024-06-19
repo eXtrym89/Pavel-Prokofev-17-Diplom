9033ebfde7f40bf732d3619b2d7278212ae4bc4a
eXtrym89 <mpbf2013@gmail.com>
2024-06-19 12:08:45 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://807d0419-3c9c-4629-8042-08bd5873e2fb.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ request.py
@@ -0,0 +1,9 @@
+import configuration
+import requests
+import data
+
+def post_new_order():
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_NEW_ORDER,
+                         json=data.order_body)
+def get_orders_track(track_id):
+    return requests.get(configuration.URL_SERVICE + configuration.RECEIVING_ORDER_BY_ID + "?t=" + str(track_id))
